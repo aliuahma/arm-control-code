ba1a59f61068ac34e51a9c6b3e1758849f026046
Ali Ahmad <57443539+aliuahma@users.noreply.github.com>
2024-08-02 08:41:55 -0700

    Upload from VEXCode project

---
+++ include/arm.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include "vex.h"
+
+class Arm {
+
+public:
+
+  Arm(const std::vector<vex::motor*>& arms, float kP, float kD);
+  ~Arm();
+  void setAngle(float desiredAngle);
+  static int armControlWrapper();
+
+private:
+  
+  std::vector<vex::motor*> _arms;
+  float _desiredAngle;
+  float _kP;
+  float _kD;
+  static Arm* _instance;
+  bool _stopTask;
+  vex::task* _armTask;
+
+}; // class Arm
---
+++ src/arm.cpp
@@ -0,0 +1,50 @@
+#include "arm.h"
+
+Arm* Arm::_instance = nullptr;
+
+Arm(const std::vector<vex::motor*>& arms, float kP, float kD) {
+  _arms = arms;
+  _desiredAngle = 0.0; // default state
+  _kP = kP;
+  _kD = kD;
+  _instance = this;
+  _stopTask = false;
+  _armTask = new vex::task(armControlWrapper);
+} 
+
+Arm::~Arm() {
+  _stopTask = true;
+  _armTask->stop();
+  delete _armTask;
+}
+
+void Arm::setAngle(float desiredAngle) {
+  _desiredAngle = desiredAngle;
+} 
+
+int Arm::armControl() {
+  float lastError = 0;
+  while(!_stopTask) {
+    float currentAngle = 0;
+    for (auto arm : _arms) {
+      currentAngle += arm->position(deg);
+    }
+    currentAngle /= _arms.size();
+    float error = _desiredAngle - currentAngle;
+    float derivative = error - lastError;
+    float armPower = error * _kP + derivative * _kD;
+    for (auto arm : _arms) {
+      arm->spin(vex::directionType::fwd, armPower, vex::voltageUnits::mV);
+    }
+    lastError = error;
+    vex::task::sleep(10);
+  }
+  for (auto arm : _arms) {
+    arm->setBrake(brake);
+  }
+  return 1;
+} 
+
+int Arm::armControlWrapper() {
+  return _instance->armControl();  
+}
