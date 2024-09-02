28a08807b48a45c3b2fc934ad3e9cd9c965e1442
Ali Ahmad <57443539+aliuahma@users.noreply.github.com>
2024-09-02 15:38:11 +0200

    Remove comment bloat

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -2,18 +2,12 @@
 
 using namespace vex;
 
-// A global instance of brain used for printing to the V5 brain screen
 brain Brain;
 
 motor liftArm1 = motor(PORT1, ratio36_1, true);
 motor liftArm2 = motor(PORT2, ratio36_1, true);
 Arm arm = Arm(&liftArm1, &liftArm2, 1.0, 0.1);
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
- * This should be called at the start of your int main function.
- */
 void vexcodeInit(void) {
   // Nothing to initialize
 }
