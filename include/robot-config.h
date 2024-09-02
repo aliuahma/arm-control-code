6db9118e30b39860bffcac16dad8f3a615bc35a1
Ali Ahmad <57443539+aliuahma@users.noreply.github.com>
2024-09-02 15:37:43 +0200

    Remove comment bloat

--- include/robot-config.h
+++ include/robot-config.h
@@ -2,11 +2,6 @@ using namespace vex;
 
 extern brain Brain;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
- * This should be called at the start of your int main function.
- */
 void vexcodeInit(void);
 
 extern motor liftArm1;
